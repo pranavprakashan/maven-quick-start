612f0804b1058698c1ff79ce5f4a918265138055
91984 <pranavprakashan07@gmail.com>
2021-02-25 15:34:36 +0530

    modified application and applicationtest

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,32 +3,31 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
-
 public class Application {
-public int countWords(String words){
-String[] separateWords = StringUtils.split(words, ' ');
-return (separateWords == null) ? 0 :separateWords.length;
-}
+    public int countWords(String words) {
+        String[] separateWords = StringUtils.split(words, " ");
+        return (separateWords == null)? 0 :separateWords.length;
+    }
 
-public void greet(){
-List<String> greetings = new ArrayList<>();
-greetings.add("Hello");
+    public void greet() {
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Hello");
 
-for (String greeting : greetings){
-System.out.println ("Greeting: " + greeting);
-}
-}
+        for (String greeting : greetings) {
+            System.out.println("Greetings: " + greeting);
+        }
+    }
 
-public Application() {
-System.out.println ("Inside Application");
-}
+    public Application() {
+        System.out.println ("Inside Application");
+    }
 
-// method main(): ALWAYS the APPLICATION entry point
-public static void main (String[] args) {
-System.out.println ("Starting Application");
-Application app = new Application();
-app.greet();
-int count = app.countWords("I have four words");
-System.out.println ("Word Count: "+ count);
-}
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	    Application app = new Application();
+        app.greet();
+        int count = app.countWords("I have four words");
+        System.out.println ("words count: " + count);
+    }
 }
